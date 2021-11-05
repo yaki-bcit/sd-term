9a9fd4b2abf013f4914f964a6bf670e8482797a9
Yevgeniy Akimenko <akimenko.y@gmail.com>
2021-11-04 17:19:38 -0700

    Nov 4 class

--- starter/IOhandler.js
+++ starter/IOhandler.js
@@ -9,7 +9,7 @@
  */
 
 const unzipper = require('unzipper'),
-  fs = require("fs"),
+  fs = require('fs'),
   PNG = require('pngjs').PNG,
   path = require('path');
 
@@ -22,33 +22,89 @@ const unzipper = require('unzipper'),
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
-
+  fs.createReadStream(pathIn)
+    .pipe(unzipper.Extract({path: pathOut}))
+    .on('finish', () => console.log('Extraction operation complete'));
 };
 
+/* unzip(`${__dirname}/myfile.zip`, `${__dirname}/unzipped`); */
+
 /**
- * Description: read all the png files from given directory and return Promise containing array of each png file path 
- * 
+ * Description: read all the png files from given directory  
+ * and return Promise containing array of each png file path
  * @param {string} path 
  * @return {promise}
  */
 const readDir = dir => {
+  fs.readdir(dir, (err, files) => {
+    if (err) { 
+      console.log(err) 
+    } else {
+      console.log(files);
+    }
+  });
 
+};
+
+/* readDir(`${__dirname}/unzipped`); */
+
+const grayScaleAlg = () => {
+  const grayScaleValue = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+  // invert color
+  this.data[idx] = grayScaleValue;
+  this.data[idx + 1] = grayScaleValue;
+  this.data[idx + 2] = grayScaleValue;
+
+  this.pack().pipe(fs.createWriteStream("out.png"));
 };
 
 /**
  * Description: Read in png file by given pathIn, 
  * convert to grayscale and write to given pathOut
  * 
- * @param {string} filePath 
- * @param {string} pathProcessed 
+ * @param {array} files 
+ * @param {string} pathOut 
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {
+const grayScale = (files, pathOut) => {
+  for (file of files) {
+    if (file.endsWith('.png')) {
+      fs.createReadStream(file)
+        .pipe(
+          new PNG({
+            filterType: -1,
+          })
+          .on('parsed', function() {
+            /* for (var y = 0; y < this.height; y++) {
+              for (var x = 0; x < this.width; x++) {
+                console.log(this.data)
+                var idx = (this.width * y + x) << 2;
+         
+                // invert color
+                this.data[idx] = 255 - this.data[idx];
+                this.data[idx + 1] = 255 - this.data[idx + 1];
+                this.data[idx + 2] = 255 - this.data[idx + 2];
+         
+                // and reduce opacity
+                this.data[idx + 3] = this.data[idx + 3] >> 1;
+              }
+            } */
+            console.log(this.data);
+            this.pack().pipe(fs.createWriteStream("out.png"));
 
+          })
+        )
+    }
+  }
+  
 };
+
+grayScale(['./unzipped/1.png',], 'a');
 
 module.exports = {
   unzip,
   readDir,
   grayScale
 };
+
+
--- starter/main.js
+++ starter/main.js
@@ -3,14 +3,18 @@
  * File Name: main.js
  * Description: 
  * 
- * Created Date: 
- * Author:
+ * Created Date: November 4, 2021
+ * Author: Yevgeniy Akimenko
  * 
  */
+
+
 
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
-
+IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then(() => IOhandler.readDir(pathUnzipped))
+  .then((fileList) => IOhandler.grayScale(fileList))
